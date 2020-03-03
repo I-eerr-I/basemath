d60888324ee017bebd26ccd540ddab4f33082b57
eerne <jokey13@bk.ru>
2020-03-03 22:51:26 +0300

    Local commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name                          = "basemath",
+    version                       = "1.0",
+    author                        = "eerne/m13kj",
+    author_email                  = "infinitumco666@gmail.com",
+    description                   = "A small library to work with numbers of any base",
+    long_description              = long_description,
+    long_description_content_type = "text/markdown",
+    url                           = "https://github.com/m13kj/basemath",
+    packages                      = setuptools.find_packages(),
+    classifiers                   = [
+        "Programming Language :: Python :: 3",
+        "Licence :: -13kj License",
+        "Operating System :: OS Independent",
+        ],
+    python_requires = ">=3.6",
+    )
